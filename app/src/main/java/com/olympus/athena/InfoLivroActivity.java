2fc58467717e900e01ae89bd8a0aaf39d4c0548a
20201TIIMI0349 <jordanalsantos@hotmail.com>
2022-08-26 09:01:50 -0300

    criação de cada layout

---
+++ app/src/main/java/com/olympus/athena/InfoLivroActivity.java
@@ -0,0 +1,14 @@
+package com.olympus.athena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoLivroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_livro);
+    }
+}
---
+++ app/src/main/java/com/olympus/athena/ListaLivrosActivity.java
@@ -0,0 +1,14 @@
+package com.olympus.athena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaLivrosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_livros);
+    }
+}
