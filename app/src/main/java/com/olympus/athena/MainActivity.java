2a27ddbb8076da044dc63cd1ee2880c9131365a9
20201TIIMI0012 <brenoaaffonso@gmail.com>
2022-11-04 12:20:14 -0300

    criação de fragments e algumas outras alterações

--- app/src/main/java/com/olympus/athena/MainActivity.java
+++ app/src/main/java/com/olympus/athena/MainActivity.java
@@ -1,6 +1,8 @@
 package com.olympus.athena;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // setando a action bar da tela
+        Toolbar tbMain = findViewById(R.id.tbMain);
+        setSupportActionBar(tbMain);
+    }
+
+    public void setActivityTitle(String title) {
+        //método para setar o nome da activity
+        setTitle(title);
     }
 }
