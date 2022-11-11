c96564747be30d59301b0276806da93e8655b11b
20201TIIMI0349 <jordanalsantos@hotmail.com>
2022-11-11 09:10:56 -0300

    edição dos nomes

--- app/src/main/java/com/olympus/athena/BookCatFragment.java
+++ app/src/main/java/com/olympus/athena/BookCatFragment.java
@@ -59,6 +59,6 @@ public class BookCatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_cat, container, false);
+        return inflater.inflate(R.layout.fragment_categoria, container, false);
     }
 }
--- app/src/main/java/com/olympus/athena/BookListFragment.java
+++ app/src/main/java/com/olympus/athena/BookListFragment.java
@@ -59,6 +59,6 @@ public class BookListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_list, container, false);
+        return inflater.inflate(R.layout.fragment_lista_livro, container, false);
     }
 }
